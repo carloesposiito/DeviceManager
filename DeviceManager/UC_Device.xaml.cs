28b9159824817126ae1513d1e83689e96670d6e9
Lenovo-Carlo <carloesposito.c@gmail.com>
2025-03-09 19:41:40 +0100

    Restored UC_Device user control.

---
+++ DeviceManager/UC_Device.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PlatformTools;
+using System.Windows.Controls;
+
+namespace DeviceManager
+{
+    public partial class UC_Device : UserControl
+    {
+        #region "Private variables"
+
+        private Device _device;
+
+        #endregion
+
+        #region "Properties"
+
+        /// <summary>
+        /// Device object.
+        /// </summary>
+        public Device Device { get => _device; }
+
+        #endregion
+
+        public UC_Device(Device device)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            _device = device;
+        }
+    }
+}
